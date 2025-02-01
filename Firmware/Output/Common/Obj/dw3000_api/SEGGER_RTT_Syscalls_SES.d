b73d80347ebc4a1018733340fc899b2f2e1e54aa
Walter Tebbetts <tebbettswt@gmail.com>
2025-02-01 15:58:19 -0500

    moved project build off of Docker, and simplified files

---
+++ Firmware/Output/Common/Obj/dw3000_api/SEGGER_RTT_Syscalls_SES.d
@@ -0,0 +1,12 @@
+Output/Common/Obj/dw3000_api/SEGGER_RTT_Syscalls_SES.o: \
+ /home/waltertebbetts/Documents/SDP/BareTag_Tool_Tracker/Firmware/Src/SEGGER/SEGGER_RTT_Syscalls_SES.c \
+ /home/waltertebbetts/Documents/SDP/BareTag_Tool_Tracker/Firmware/Src/SEGGER/SEGGER_RTT.h \
+ /home/waltertebbetts/Documents/SDP/BareTag_Tool_Tracker/Firmware/Src/SEGGER/SEGGER_RTT_Conf.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/stdlib.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/__crossworks.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/stdarg.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/stdio.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/limits.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/__libc.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/__vfprintf.h \
+ /usr/local/segger_embedded_studio_V5.42a/include/__argtype.h
---
+++ Firmware/Output/Common/Obj/dw3000_api/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Common/Obj/dw3000_api/ses_startup_nrf_common.o: \
+ /usr/local/nRF5_SDK_17.1.0_ddde560/modules/nrfx/mdk/ses_startup_nrf_common.s
